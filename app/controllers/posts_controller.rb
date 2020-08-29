3b39c0e283e7216e9a5764696c2b14baf9dd107f
Masatoshi <masa@example.com>
2020-08-29 18:14:42 +0900

    second_commit

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,19 @@
+# frozen_string_literal: true
+
 class PostsController < ApplicationController
+  def index
+    @posts =Post.all
+  end
+
+  def new
+    @post=Post.new
+  end
+
+  def create
+    @post=Post.new(post_params)
+    if @post.save
+      redirect_to
+    end
+  end
+
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,10 @@
-class Post < ApplicationRecord
+# frozen_string_literal: true
+#記事投稿
 
-  validates
+class Post < ApplicationRecord
+  has_many :comments
+
+  validates :user_name, presence: true, length: {in: 2..10}
+  validates :title    , presence: true, length: {in: 4..20}
+  validate  :content,   presence: true, length: {in: 5..100}
 end
